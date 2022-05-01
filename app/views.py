0bb3cba48fc691f1601d0cc254437ea4c794bdc4
MugureNjunge <maureennjunge22@gmail.com>
2022-05-01 17:25:51 +0300

    add the readme file

--- app/views.py
+++ app/views.py
@@ -3,5 +3,8 @@ from app import app
 
 @app.route('/')
 def index():
-  
-  return render_template('index.html')
+
+    return render_template('index.html')
+
+  # return render_template('navbar.html')
+
