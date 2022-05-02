000e2d89c4df75fbbc1d7fcf387ca3d4ba19782f
MugureNjunge <maureennjunge22@gmail.com>
2022-05-02 14:21:37 +0300

    add the app search route

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from app import app
-from .request import get_news
-from .request import get_sources
+from .request import get_news,get_news
+# from .request import get_sources
 
 @app.route('/')
 
@@ -10,10 +10,22 @@ def index():
   news = get_news()
   return render_template('index.html',articles= news)
 
-def sources():
+@app.route('/search/<article_name>')
+def search(article_name):
+    '''
+    View function to display the search results
+    '''
+    article_name_list = article_name.split(" ")
+    article_name_format = "+".join(article_name_list)
+    searched_articles = searched_articles(article_name_format)
+    title = f'search results for {article_name}'
+    return render_template('search.html',movies = searched_articles)
+  
 
-  sources = get_sources()
-  return render_template('sources.html', sources= sources)
+# def sources():
+
+#   sources = get_sources()
+#   return render_template('sources.html', sources= sources)
 
 
 
