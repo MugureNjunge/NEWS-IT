5917e88b2747d4cc98ef45e51fd9fb0cfe01496a
MugureNjunge <maureennjunge22@gmail.com>
2022-05-02 09:52:58 +0300

    import the get sources function

--- app/request.py
+++ app/request.py
@@ -38,34 +38,3 @@ def process_results(news_list):
   return news_results  
 
 
-def get_sources():
-    get_sources_url = sources_url.format(api_key)
-
-  with urllib.request.urlopen(get_sources_url) as url:
-    get_news_data = url.read()
-    get_news_response = json.loads(get_sources_data)
-
-    news_results = None
-
-    if get_sources_response['articles']:
-      news_results_list = get_news_response['articles']
-      news_results = process_results(news_results_list)
-  return news_results
-
-def process_results(news_list):
-  news_results = []
-  for news_item in news_list:
-    author = news_item.get("author")
-    title = news_item.get("title")
-    description = news_item.get("description")
-    url = news_item.get("url")
-    urlToImage = news_item.get("urlToImage")
-    publishedAt = news_item.get("publishedAt")
-    content = news_item.get("content")
-
-    news_object = News(author, title, description, url, urlToImage, publishedAt, content)
-    news_results.append(news_object)
-  return news_results  
-
-
-
--- app/views.py
+++ app/views.py
@@ -1,14 +1,18 @@
 from flask import render_template
 from app import app
 from .request import get_news
+from .request import get_sources
 
 @app.route('/')
 def index():
 
-
-
   news = get_news()
   return render_template('index.html',articles= news)
+
+def sources():
+
+  sources = get_sources()
+  return render_template('sources.html', articles= sources)
 
 
 
